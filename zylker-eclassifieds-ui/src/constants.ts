5203361c84cd266fb8bfe105abc5227bdbcf34ed
Priyatharshini-J <75873426+Priyatharshini-J@users.noreply.github.com>
2025-07-03 11:11:20 +0530

    Update constants.ts

--- zylker-eclassifieds-ui/src/constants.ts
+++ zylker-eclassifieds-ui/src/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = window.location.origin;
 export const STRATUS_BUCKET_NAME = 'YOUR BUCKET NAME';
 export const CLIENTID = 'YOUR CLIENT ID'; 
+export const STRATUS_BUCKET_URL = 'YOUR BUCKET URL';
